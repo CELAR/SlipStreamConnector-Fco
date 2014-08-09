122c341ba8226cd99c3a05f82e742aa8f29087c9
Konstantin Skaburskas <konstantin.skaburskas@gmail.com>
2014-08-09 17:22:07 +0200

    Minor.

--- java/jar/src/main/java/com/sixsq/slipstream/connector/flexiant/FCOSystemConfigurationParametersFactory.java
+++ java/jar/src/main/java/com/sixsq/slipstream/connector/flexiant/FCOSystemConfigurationParametersFactory.java
@@ -9,9 +9,9 @@ package com.sixsq.slipstream.connector.flexiant;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,7 +29,7 @@ public class FCOSystemConfigurationParametersFactory extends
 		SystemConfigurationParametersFactoryBase {
 
 	private final static Logger log = Logger.getLogger(FCOSystemConfigurationParametersFactory.class.getName());
-	
+
 	public FCOSystemConfigurationParametersFactory(String connectorInstanceName)
 			throws ValidationException {
 		super(connectorInstanceName);
@@ -38,17 +38,17 @@ public class FCOSystemConfigurationParametersFactory extends
 	protected void initReferenceParameters() throws ValidationException {
 
 		super.initReferenceParameters();
-		
+
 		log.info("In initReferenceParameters()");
-		
-		// We just need an end-point, so can just use the inherited method instead of 
+
+		// We just need an end-point, so can just use the inherited method instead of
 		// using putMandatoryParameter() explicitly
-		putMandatoryEndpoint();	
-		
+		putMandatoryEndpoint();
+
 //		putMandatoryParameter(
 //		constructKey(FCOUserParametersFactory.FCO_USER_ENDPOINT),
 //		"FCO User API Endpoint", "https://cp.sd1.flexiant.net:4442/");
-		
+
 //		putMandatoryOrchestratorInstanceType();
 //
 		putMandatoryParameter(constructKey("update.clienturl"),
@@ -57,7 +57,7 @@ public class FCOSystemConfigurationParametersFactory extends
 //		putMandatoryParameter(
 //				constructKey(FCOUserParametersFactory.SECURITY_GROUP_PARAMETER_NAME),
 //				"Orchestrator security group", "default");
-		
+
 //		putMandatoryParameter(
 //				constructKey(FCOUserParametersFactory.FCO_ADMIN_ENDPOINT),
 //				"FCO Admin Endpoint", "https://cp.sd1.flexiant.net:4443/");
