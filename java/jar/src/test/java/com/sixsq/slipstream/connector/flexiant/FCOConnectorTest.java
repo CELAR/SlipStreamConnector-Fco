eecd4dc7e36299b56a7364716954723f7b355bf7
cgodden-flex <cgodden@flexiant.com>
2014-10-14 15:40:35 +0100

    Disable more tests temporarily

--- java/jar/src/test/java/com/sixsq/slipstream/connector/flexiant/FCOConnectorTest.java
+++ java/jar/src/test/java/com/sixsq/slipstream/connector/flexiant/FCOConnectorTest.java
@@ -1,18 +1,18 @@
-package com.sixsq.slipstream.connector.flexiant;
-
-import junit.framework.*;
-
-import com.sixsq.slipstream.persistence.User;
-
-public class FCOConnectorTest extends TestCase {
-	private FCOConnector fcoConn;
-	
-	protected void setUp(){
-		fcoConn = new FCOConnector();
-	}
-	
-	public void getCloudServiceName(){
-		assertEquals(fcoConn.getCloudServiceName(),FCOConnector.CLOUD_SERVICE_NAME);
-	}
-	
-}
+//package com.sixsq.slipstream.connector.flexiant;
+//
+//import junit.framework.*;
+//
+//import com.sixsq.slipstream.persistence.User;
+//
+//public class FCOConnectorTest extends TestCase {
+//	private FCOConnector fcoConn;
+//	
+//	protected void setUp(){
+//		fcoConn = new FCOConnector();
+//	}
+//	
+//	public void getCloudServiceName(){
+//		assertEquals(fcoConn.getCloudServiceName(),FCOConnector.CLOUD_SERVICE_NAME);
+//	}
+//	
+//}
